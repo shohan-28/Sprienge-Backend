2a72d44fa1d6c5f7d686910b234b279da10d0b1d
shohan-28 <shohanshawon551@gmail.com>
2026-09-19 22:44:33 +0600

    আপনার পরিবর্তন সম্পর্কিত একটি ছোট মেসেজ

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
+  "https://bdmart-mu.vercel.app/",
 
   // Admin Panel
   "https://sprienge-admin-panel.vercel.app",
